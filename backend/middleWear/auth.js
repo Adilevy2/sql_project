54c2ac44bd29d52687e3abe44bc579fa9421ddec
adi levy <adilevy156@gmail.com>
2023-01-24 15:07:22 +0200

    last commit

--- backend/middleWear/auth.js
+++ backend/middleWear/auth.js
@@ -5,7 +5,6 @@ module.exports = function auth(req,res,next) {
     const token = req.header('x-auth-token')
     if (!token) 
         return res.status(401).send('access denied no token provided')
-
     try{
         const decoded = jwt.verify(token,'tokenWord');
         req.user = decoded;
